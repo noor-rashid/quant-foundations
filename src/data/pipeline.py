bd8aee875b72b0be7bfe5cc5098d84295d2580ad
Noor Rashid <nrashid@Noors-MacBook-Pro.local>
2026-02-06 18:24:09 +0000

    Add data data caching

---
+++ src/data/pipeline.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+import datetime
+from fetchers import fetch_ohlcv
+import pandas as pd
+
+def get_ohlcv(output_dir: str, symbol: str, interval: str, start: str, end: str) -> pd.DataFrame:
+    """
+    check if cache exists and call if it does, or make API request if not
+
+    :param symbol:
+    :param interval:
+    :param start:
+    :param end:
+    :return:
+    """
+    filename = f"binance_daily_data_{symbol}_{interval}_{start}_{end}.csv"
+    filepath = Path(output_dir) / filename
+
+    if not os.path.exists(filepath):
+        print("Filepath does not exist, hitting API Endpoint and fetching new data")
+        data = fetch_ohlcv(symbol, interval, start, end)
+        save_data = data.to_csv(filepath, index=False)
+        return data
+    else:
+        print("Filepath Exists, fetching cached data")
+        data = pd.read_csv(filepath)
+        return data
+
+
+
